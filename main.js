3aa19d67b51fbc444b8c2676bad0e21ff6681de8
Victor Costa <littledeveloperman@gmail.com>
2022-09-22 15:07:11 -0300

    componente header atualizado, foi criado a função scroll quando escrolar para alteral as cores.

---
+++ main.js
@@ -0,0 +1,7 @@
+function onScroll() {
+    if(scrollY > 0) {
+        document.getElementById('navigation').classList.add('scroll');
+    }else{
+        document.getElementById('navigation').classList.remove('scroll');
+    }
+}
