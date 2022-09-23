58e0406ce15a14ec3e02cdd9476a130c8356512e
Victor Costa <littledeveloperman@gmail.com>
2022-09-23 20:38:50 -0300

    Novas atualizações no menu principal.

--- main.js
+++ main.js
@@ -5,3 +5,12 @@ function onScroll() {
         document.getElementById('navigation').classList.remove('scroll');
     }
 }
+
+function onClick() {
+    if(true) {
+        document.getElementById('corpo').classList.add('menu-expanded');
+    }
+    else if(true) {
+        document.getElementById('corpo').classList.remove('menu-expanded');
+    }
+}
